03f5ccf6bfc4db4acec29985d7f33c730894b586
unknown <antaresax@gmail.com>
2018-06-06 17:22:47 +0900

    mail log set

--- public/js/DRAW.Framework.js
+++ public/js/DRAW.Framework.js
@@ -15,92 +15,77 @@ function touchPoint(event, target) {
 
 DRAW.Framework =new function(){
 
-
     //DRAW.Framework.RandomSet();
-      this.RandomSet = function() {
+    this.RandomSet = function() {
+        //color pitch 랜덤
+        var RandomValue =Math.floor(Math.random() * 13)
+           ,ColorList=["#57009f","#740000","#b30000","#ed0000","#ff6300",
+                        "#ffeb00","#9dff00","#29ff00","#00ffe3","#007dff","#4700e4","#4400eb"]
+           ,PitchList=[349,370,392,415,440,466,493,523,554,587,622,659];
 
-                //color pitch 랜덤
-            var RandomValue =Math.floor(Math.random() * 13)
-               ,ColorList=["#57009f","#740000","#b30000","#ed0000","#ff6300",
-                            "#ffeb00","#9dff00","#29ff00","#00ffe3","#007dff","#4700e4","#4400eb"]
-               ,PitchList=[349,370,392,415,440,466,493,523,554,587,622,659];
+        var RandomPitchSetValue =Math.floor(Math.random() * 4)
+        var ExtendPitchData;
 
-            var RandomPitchSetValue =Math.floor(Math.random() * 4)
-            var ExtendPitchData;
+        switch(RandomPitchSetValue) {
+            case 0:
+                ExtendPitchData=PitchList[RandomValue]/2
+               break;
+            case 1:
+                ExtendPitchData=PitchList[RandomValue]*1
+               break;
+            case 2:
+               ExtendPitchData=PitchList[RandomValue]*2
+               break;
+            case 3:
+               cExtendPitchData=PitchList[RandomValue]*3
+               break;
+         }
 
+            // width 랜덤
+        var RandomColorBlur =Math.floor(Math.random() * 13)
+        var RandomWidth =Math.floor(Math.random() * 45)+5;
+        var RandomBlur =Math.floor(Math.random() * 200)
 
-                         switch(RandomPitchSetValue) {
-                           case 0:
-                               ExtendPitchData=PitchList[RandomValue]/2
-                               break;
-                           case 1:
-                              ExtendPitchData=PitchList[RandomValue]*1
-                               break;
-                           case 2:
-                               ExtendPitchData=PitchList[RandomValue]*2
-                               break;
-                           case 3:
-                               cExtendPitchData=PitchList[RandomValue]*3
-                               break;
-                         }
+        console.log(RandomWidth);
+        console.log(RandomBlur);
 
-
-
-
-
-                // width 랜덤
-            var RandomColorBlur =Math.floor(Math.random() * 13)
-            var RandomWidth =Math.floor(Math.random() * 45)+5;
-            var RandomBlur =Math.floor(Math.random() * 200)
-
-               console.log(RandomWidth);
-               console.log(RandomBlur);
-
-                return (
-
-                [ColorList[RandomValue],ExtendPitchData
-                ,RandomValue,RandomWidth,RandomBlur,ColorList[RandomColorBlur]]
-                )
-
-
-      }
+        return (
+            [ColorList[RandomValue],ExtendPitchData
+            ,RandomValue,RandomWidth,RandomBlur,ColorList[RandomColorBlur]]
+        )
+    }
 
     //DRAW.Framework.BackGroundSet();
-
     this.BackGroundSet = function() {
 
-      var RandomValue =Math.floor(Math.random() * 13)
+        var RandomValue =Math.floor(Math.random() * 13)
         var RandomValueOne =Math.floor(Math.random() * 13)
-          var RandomValueTwo=Math.floor(Math.random() * 13)
-              ,ColorList=["#57009f","#740000","#b30000","#ed0000","#ff6300",
+        var RandomValueTwo=Math.floor(Math.random() * 13)
+            ,ColorList=["#57009f","#740000","#b30000","#ed0000","#ff6300",
                       "#ffeb00","#9dff00","#29ff00","#00ffe3","#007dff","#4700e4","#4400eb"]
 
-          $("canvas").css(
+        $("canvas").css(
             "background"
-            ,"radial-gradient("+ColorList[RandomValue]+""
+           ,"radial-gradient("+ColorList[RandomValue]+""
             +" 10%,"+ColorList[RandomValueOne]+""
-            +" 35%,"+ColorList[RandomValueTwo]+""+"  60%)");
+            +" 35%,"+ColorList[RandomValueTwo]+""+"  60%)"
+        );
 
+        return (
+             [ColorList[RandomValue],ColorList[RandomValueOne],ColorList[RandomValueTwo]]
+        )
+    }
 
-
-              return (
-              [ColorList[RandomValue],ColorList[RandomValueOne],ColorList[RandomValueTwo]]
-              )
-
-      }
-
-  //DRAW.Framework.FmCarrierType();
-
-      this.FmCarrierType = function() {
+    //DRAW.Framework.FmCarrierType();
+    this.FmCarrierType = function() {
 
         var RandomValue =Math.floor(Math.random() * 4)
             ,CarrierTypeList=["sine","sawtooth","triangle","square"]
+        //console.log(CarrierTypeList[RandomValue]);
+        return (
+            CarrierTypeList[RandomValue]
+        )
 
-       console.log(CarrierTypeList[RandomValue]);
-                return (
-                CarrierTypeList[RandomValue]
-                )
-
-        }
+    }
 
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,27 @@
 
-   var DSX = new DSX;
-    ;(function (window, Constructor, undefined) {
+ var DSX = new DSX;
+;(function (window, Constructor, undefined) {
 
     // Constructor
     var mainDrawPaint = function(){
          that = this;
     };
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+    // 변수를 선언합니다.
 
-     var canvas = document.getElementById('canvas');
-     var context = canvas.getContext('2d');
-     // 변수를 선언합니다.
-
-
-     // Make sure the image is loaded first otherwise nothing will draw.
-
+    // Make sure the image is loaded first otherwise nothing will draw.
 
     //  var width = 30;
     //  var color = color;
     //  var isDown = false;
     //  var blurColor = "#ffe6e6";
     //  var blurWidth = 150
-     var newPoint, oldPoint;
-     var socket = io.connect();
+    var newPoint, oldPoint;
+    var socket = io.connect();
 
-
-     // Socket 데이터 값 받기
-       socket.on('line', function(data) {
+    // Socket 데이터 값 받기
+    socket.on('line', function(data) {
            context.lineWidth = data.width;
            context.strokeStyle = data.color;
 
@@ -40,100 +36,93 @@
 
 
        });
-       socket.on('clear', function(data) {
-          console.log(data.clear);
+    socket.on('clear', function(data) {
+          //console.log(data.clear);
            DRAW.CLEAR =data.clear
        });
 
-
-
     mainDrawPaint.prototype = {
-     init : function() {
+        init : function() {
 
-                  //  $(window).resize(function() {
-                  //      $('canvas').css('width', $(window).width());
-                  //  });
-
-                    that.preset.soundControl();
-                      DRAW.Framework.BackGroundSet(); //배경화면 자동변경
-
-
-     }
-
-     ,preset :{
-
-       soundControl:function(){
-
-
-              var fm = new  DSX.FM({
-                            carrier_type:"sine"
-                            ,carrier:500
-                            ,mod_type :"sine"
-                            ,modfreq:700
-                            ,depth:800
-                            ,gain:0.5
-
-                                  });
-         //var osc = new DSX.Osc({type:"sawtooth",freq: 400});
-         var gain = new DSX.Amp({ gain: 0.0});
-
-            fm.connect(gain);
-            gain.connect(DAC);
-
-
-
-          //화면 지우기
-          socket.on('clear', function(data) {
-             console.log(data.clear);
-              DRAW.CLEAR =data.clear
-            context.clearRect(0, 0, canvas.width, canvas.height);
-            DRAW.Framework.BackGroundSet(); //배경화면 자동 변경
-          });
-
-        // 좌표값에 따라 피치 바꾸기
-        socket.on('line', function (data) {
-
-            //osc.freq =data.pitch;
-            fm.carrier=data.pitch;
-            fm.modfreq=data.pitchOne*400;
-            fm.depth=data.pitchTwo*100;
-              //osc.freq =data.pitchOne; // 붓 사이즈
-                //osc.freq =data.pitchTwo; // 블러 크기
-
-            console.log(data.pitchOne ,data.pitchTwo);
-
-            // 캐리어 데이터 변경
-              var CarrierDate =DRAW.Framework.FmCarrierType()
-
-              fm.carrier_type=CarrierDate;
-
-              fm.mod_type=CarrierDate;
-
-
-              });
-
-        // 그리기시작하면 엠프값설정
-        socket.on('start', function (data) {
-
-            gain.gain =0.5;
-
-              });
-
-        // 그리기시작하면 엠프값설정
-        socket.on('end', function (data) {
-
-          gain.gain =0.0;
-
-              });
-
+            //  $(window).resize(function() {
+            //      $('canvas').css('width', $(window).width());
+            //  });
+            that.preset.soundControl();
+            DRAW.Framework.BackGroundSet(); //배경화면 자동변경
         }
-    }
+
+       ,preset :{
+            soundControl:function(){
+                var fm = new  DSX.FM({
+                        carrier_type:"sine"
+                        ,carrier:500
+                        ,mod_type :"sine"
+                        ,modfreq:700
+                        ,depth:800
+                        ,gain:0.5
+                  });
+                 //var osc = new DSX.Osc({type:"sawtooth",freq: 400});
+                 var gain = new DSX.Amp({ gain: 0.0});
+
+                 fm.connect(gain);
+                 gain.connect(DAC);
+
+
+                  //화면 지우기
+                  socket.on('clear', function(data) {
+                      //console.log(data.clear);
+                      DRAW.CLEAR =data.clear
+                      context.clearRect(0, 0, canvas.width, canvas.height);
+                      DRAW.Framework.BackGroundSet(); //배경화면 자동 변경
+                  });
+                // 좌표값에 따라 피치 바꾸기
+                socket.on('line', function (data) {
+
+                    //osc.freq =data.pitch;
+                    fm.carrier=data.pitch;
+                    fm.modfreq=data.pitchOne*400;
+                    fm.depth=data.pitchTwo*100;
+                    //osc.freq =data.pitchOne; // 붓 사이즈
+                    //osc.freq =data.pitchTwo; // 블러 크기
+
+                    // 캐리어 데이터 변경
+                    var CarrierDate =DRAW.Framework.FmCarrierType()
+                    fm.carrier_type=CarrierDate;
+                    fm.mod_type=CarrierDate;
+
+                      // carrier_type:"sine"
+                      // ,carrier:500
+                      // ,mod_type :"sine"
+                      // ,modfreq:700
+                      // ,depth:800
+                      // ,gain:0.5
+
+                      console.log("carrier_type",fm.carrier_type.type);
+                      console.log("carrier" ,fm.carrier.value);
+                      console.log("mod_type" ,fm.mod_type.type);
+                      console.log("modfreq",fm.modfreq.value);
+                      console.log("depth" ,fm.depth.value);
+
+
+                });
+
+                // 그리기시작하면 엠프값설정
+                socket.on('start', function (data) {
+                    gain.gain =0.5;
+                });
+                // 그리기시작하면 엠프값설정
+                socket.on('end', function (data) {
+                  gain.gain =0.0;
+                });
+            }
+        }
 
     };  //DrawPaint.prototype  end
 
-     Constructor.Set = mainDrawPaint;
-     mainDrawPaint();
-   })(window, mainPaint);
+    Constructor.Set = mainDrawPaint;
+    mainDrawPaint();
+
+})(window, mainPaint);
 
     var mainData = new mainPaint.Set() // 객체필요
     mainData.__proto__.init(); //초기 로드해야함
